431138993cca2a4ed22a392d433fbf9e80fd3d31
salem1535 <95876919+salem1535@users.noreply.github.com>
2023-04-28 11:28:50 +0800

    Update experiment 4 tests

--- src/Triangle.java
+++ src/Triangle.java
@@ -7,7 +7,7 @@ public class Triangle {
         double b = input.nextDouble();
         double c = input.nextDouble();
         if(a + b > c && b + c > a && a + c > b){
-            if(a == b && a == c) System.out.println("等边三角形");
+            if(a == b && b == c) System.out.println("等边三角形");
             else if(a == b || b == c || a == c) System.out.println("等腰三角形");
             else System.out.println("一般三角形");
         }
